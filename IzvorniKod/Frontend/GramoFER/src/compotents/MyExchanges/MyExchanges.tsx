5c0090d33d2eb08f26578c592551d34bd9bb8970
Jurica-Galic-FER <jurica.galic@fer.unizg.hr>
2025-01-07 14:33:21 +0100

    Frontend
    Draft MyExchanges stranice

--- IzvorniKod/Frontend/GramoFER/src/compotents/MyExchanges/MyExchanges.tsx
+++ IzvorniKod/Frontend/GramoFER/src/compotents/MyExchanges/MyExchanges.tsx
@@ -1,5 +1,6 @@
 import styles from "./MyExchanges.module.css";
 import Exchange from "../Exchange/Exchange";
+import ExchangeHistory from "../ExchangeHistory/ExchangeHistory";
 const pending_out = true;
 const pending_in = false;
 
@@ -7,37 +8,38 @@ const MyExchanges = () => {
   return (
     <>
       <div className={styles.container}>
-        <h1>Outgoing:</h1>
+        <h1 className={styles.out}>Outgoing</h1>
         <div className={styles.pending}>
           {!pending_out && <h2>There are no current outgoing exchanges</h2>}
           {pending_out && (
             <>
               <div className={styles.exchanges}>
-                <div>To:</div>
-                <div>Your:</div>
-                <div>For their:</div>
+                <div>To</div>
+                <div>Your</div>
+                <div>For their</div>
                 <div>Action</div>
               </div>
               <Exchange />
             </>
           )}
         </div>
-        <h1>Incoming:</h1>
+        <h1 className={styles.in}>Incoming</h1>
         <div className={styles.pending}>
           {!pending_in && <h2>There are no current incoming exchanges</h2>}
           {pending_in && (
             <>
               <div className={styles.exchanges}>
-                <div>From:</div>
-                <div>Their:</div>
-                <div>For Your:</div>
+                <div>From</div>
+                <div>Their</div>
+                <div>For Your</div>
                 <div>Action</div>
               </div>
               <p>adasda</p>
             </>
           )}
         </div>
-        <h1>Previous exchanges:</h1>
+        <h1 className={styles.prev}>Previous exchanges</h1>
+        <ExchangeHistory />
       </div>
     </>
   );
