5edb3fcabe151d4f5879ca858828f7f9c8262ee8
ThomasCodex2 <tomislav.perakovic@fer.unizg.hr>
2025-01-14 19:38:39 +0100

    Update Wish.java

--- IzvorniKod/Backend/src/main/java/com/example/gramofer/model/Wish.java
+++ IzvorniKod/Backend/src/main/java/com/example/gramofer/model/Wish.java
@@ -1,6 +1,7 @@
 package com.example.gramofer.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -43,6 +44,7 @@ public class Wish {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "edition_label_id")
+    @JsonManagedReference
     private Edition editionLabel;
 
     public Integer getWishId() {
