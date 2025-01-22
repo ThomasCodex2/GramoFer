5a2dd74a0e897ce0893123110587aa5e9c684e08
Jurica-Galic-FER <jurica.galic@fer.unizg.hr>
2025-01-22 21:01:19 +0100

    Frontend
    wishId => wishid za frotend (mistake?)
    fixing multiple popups at once err

--- IzvorniKod/Frontend/GramoFER/src/compotents/MyVinyls/MyVinyls.tsx
+++ IzvorniKod/Frontend/GramoFER/src/compotents/MyVinyls/MyVinyls.tsx
@@ -46,7 +46,7 @@ const API_BASE_URL = "https://gramofer.work.gd";
 
 const MyVinyls = () => {
   const [images, setImages] = useState<string[]>([]);
-  const [isAdmin, setIsAdmin] = useState<number>(0);
+  //const [isAdmin, setIsAdmin] = useState<number>(0);
   const [formData, setFormData] = useState<FormData>({
     vinylCondition: "",
     coverCondition: "",
@@ -227,24 +227,23 @@ const MyVinyls = () => {
         console.error("Error fetching vinyls:", error);
       }
     };
-    const checkAdmin = () => {
-      const A = localStorage.getItem("isAdmin");
-      if (A) {
-        setIsAdmin(parseInt(A));
-      }
-    };
-    checkAdmin();
+    // const checkAdmin = () => {
+    //   const A = localStorage.getItem("isAdmin");
+    //   if (A) {
+    //     setIsAdmin(parseInt(A));
+    //   }
+    // };
+    // checkAdmin();
     fetchVinyls();
   }, []);
   return (
     <div className={styles.container}>
-      {isAdmin && (
-        <Link to="/admin-site">
-          <button className={styles.adminButton}>
-            Temporary Admin page button
-          </button>
-        </Link>
-      )}
+      <Link to="/admin-site">
+        <button className={styles.adminButton}>
+          Temporary Admin page button
+        </button>
+      </Link>
+
       <form className={styles.listing_form} onSubmit={handleSubmit}>
         <h1>
           Add <span>YOUR</span> records here!
