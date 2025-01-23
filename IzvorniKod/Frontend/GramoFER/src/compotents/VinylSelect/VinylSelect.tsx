45fd101404343756ea5a3620b8ba4f2cef173d34
Jurica-Galic-FER <jurica.galic@fer.unizg.hr>
2025-01-23 13:06:30 +0100

    Frontend
    Testing admin authorization for admin site
    slight MyExchanges updates

--- IzvorniKod/Frontend/GramoFER/src/compotents/VinylSelect/VinylSelect.tsx
+++ IzvorniKod/Frontend/GramoFER/src/compotents/VinylSelect/VinylSelect.tsx
@@ -1,27 +1,53 @@
 import styles from "./VinylSelect.module.css";
 import VinylBox from "../VinylBox/VinylBox";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 // import { useEffect } from "react";
 // import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 const VinylSelect = () => {
   const [selectedGenre, setSelectedGenre] = useState("Rock");
   const handleGenreChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     setSelectedGenre(event.target.value);
   };
-  // const navigate = useNavigate();
-  // useEffect(() => {
-  //   const urlParams = new URLSearchParams(window.location.search);
-  //   const urltoken = urlParams.get("token");
-  //   if (urltoken) {
-  //     localStorage.setItem("aToken", urltoken);
-  //     navigate("/");
-  //     return;
-  //   }
-  // });
   const navigate = useNavigate();
+  const [isAdmin, setIsAdmin] = useState(false);
+  useEffect(() => {
+    const token = localStorage.getItem("aToken");
+    const fetchAdmin = async () => {
+      try {
+        const AdminResponse = await fetch(
+          `https://gramofer.work.gd/api/admintable/admin`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        if (AdminResponse.ok) {
+          setIsAdmin(true);
+        } else {
+          setIsAdmin(false);
+        }
+      } catch (error) {
+        setIsAdmin(false);
+        console.error(error);
+      }
+    };
+    fetchAdmin();
+  });
   return (
     <div className={styles.container}>
+      {isAdmin && (
+        <Link to="/admin-site">
+          <button className={styles.adminButton}>
+            Temporary Admin page button
+          </button>
+        </Link>
+      )}
       <div className={styles.welcome_banner}>
         <div className={styles.image_crop}></div>
         <div className={styles.banner_text}>
