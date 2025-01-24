3715186b4770d7cda680e03513a6205d32c5d09c
ThomasCodex2 <tomislav.perakovic@fer.unizg.hr>
2025-01-24 02:35:02 +0100

    Fix exchangeServicea

--- IzvorniKod/Backend/src/main/java/com/example/gramofer/repo/ExchangeRepo.java
+++ IzvorniKod/Backend/src/main/java/com/example/gramofer/repo/ExchangeRepo.java
@@ -1,6 +1,7 @@
 package com.example.gramofer.repo;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,10 +14,10 @@ import com.example.gramofer.model.UserAccount;
 @Repository
 public interface ExchangeRepo extends JpaRepository<Exchange, Integer> {
 
-    @Query("SELECT e FROM Exchange e WHERE e.user = :user AND e.status = 'active'")
+    @Query("SELECT e FROM Exchange e WHERE e.user = :user AND e.status = 'ongoing'")
     List<Exchange> findAllByUserAndStatusZero(@Param("user") UserAccount user);
 
-    @Query("SELECT e FROM Exchange e WHERE e.isOfferingUser = :isOfferingUser AND e.status = 'active'")
+    @Query("SELECT e FROM Exchange e WHERE e.isOfferingUser = :isOfferingUser AND e.status = 'ongoing'")
     List<Exchange> findAllByIsOfferingUserAndStatusZero(@Param("isOfferingUser") UserAccount isOfferingUser);
 
     @Query("SELECT e FROM Exchange e WHERE e.user = :user AND e.status = 'done'")
@@ -24,4 +25,6 @@ public interface ExchangeRepo extends JpaRepository<Exchange, Integer> {
 
     @Query("SELECT e FROM Exchange e WHERE e.isOfferingUser = :isOfferingUser AND e.status = 'done'")
     List<Exchange> findAllByIsOfferingUserAndStatusOne(@Param("isOfferingUser") UserAccount isOfferingUser);
+
+    Optional<Exchange> findById(Integer id);
 }
